33f3735652dc778e7b9440663eb931c92e51807a
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2020-10-09 04:37:56 -0700

    created a scss component for manage users

--- src/app/content/manage-user-groups/manage-user-groups.component.ts
+++ src/app/content/manage-user-groups/manage-user-groups.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-user-groups.component.scss']
 })
 export class ManageUserGroupsComponent implements OnInit {
-
+  // will change the date created to a Date type later
+  userGroups: {department: string, numOfPeople: number, dateCreated: string}[]=[
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+      {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+    {department: 'Finance Department', numOfPeople :13, dateCreated: '22-12-2009'},
+  ]
   constructor() { }
 
   ngOnInit(): void {
