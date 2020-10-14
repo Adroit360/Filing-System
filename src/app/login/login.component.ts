22f96264418b2dc47d5fe2d1c1044355bf52430d
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2020-10-14 06:24:27 -0700

    login page completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,6 +15,7 @@ export class LoginComponent implements OnInit {
     this.LogInForm = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, Validators.required),
+      rememberMe: new FormControl(null, Validators.required),
     });
   }
 
