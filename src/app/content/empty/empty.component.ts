7b1357b73fddb1ae5afa3780ce3b9f02ea1e34de
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2020-10-16 08:58:11 -0700

    folder page in session

--- src/app/content/empty/empty.component.ts
+++ src/app/content/empty/empty.component.ts
@@ -11,9 +11,20 @@ export class EmptyComponent implements OnInit {
   'Marketting', 'Accounting', 'Finance', 'Football', 'Cleaning',
   'Marketting', 'Accounting', 'Finance', 'Football', 'Cleaning',
 ];
+
+// alternate array to load the files from the server
+department_files = ['invoice.pdf', 'invoice.doc', 'invoice.txt', 'invoice.jpeg', 'invoice.png',]
+title="";
+fontIcon = "fa fa-folder";
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onFolderClicked(item: string){
+    this.title += " > "+item;
+    this.departments = [];
+    this.departments = this.department_files;
+  }
 }
