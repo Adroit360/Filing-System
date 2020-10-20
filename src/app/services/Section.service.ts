512bb7de9da62fedde6e0ed2bd314f553f9744aa
emmaofori20 <emmanuelofori2638@gmail.com>
2020-10-20 10:30:01 +0100

    renaming

--- src/app/services/Section.service.ts
+++ src/app/services/Section.service.ts
@@ -2,12 +2,7 @@ import { Subject } from 'rxjs';
 import { Section } from '../models/section.model';
 
 export class SectionService{
-  onDeleteSection(section: Section) {
-    throw new Error('Method not implemented.');
-  }
-  UpdateSection(rename: Section, index: any) {
-    throw new Error('Method not implemented.');
-  }
+
 
   // contains an array of departments created by the admin
   private sections: Section [] = [
@@ -25,36 +20,6 @@ export class SectionService{
 ];
 
 
-//  let files = {
-//     "Marketing":{
-//       "one":{
-//         "four":{},
-//         "five":{},
-//         "six":{}
-//       },
-//       "two":{
-//         "seven":{},
-//       },
-//       "three":{
-//       },
-//     },
-//     "Supply Chain":{
-
-//     },
-//     "IT":{
-
-//     },
-//     "Production":{
-
-//     },
-//     "Finance":{
-
-//     },
-//     "SharedService":{
-
-//     },
-//   }
-
   department_files = ['invoice.pdf', 'invoice.doc', 'invoice.txt', 'invoice.jpeg', 'invoice.png',]; // contains the folders and files for each department
 
   sectionName = new Subject<string>();
@@ -64,11 +29,35 @@ export class SectionService{
     return this.sections.slice();
   }
 
+//displaying a section by name
   displaysection(value:Section){
-
-  //  return value;
   this.sectionName.next(value.name);
-  console.log(value.name);
+
+  }
+
+  //update section
+  updateSection(item:Section, index){
+    if (index !=-1){
+      this.sections[index]= item
+    }
+  }
+
+  //Deleting a  section
+  onDeleteSection(section: Section) {
+    //throw new Error('Method not implemented.');
+    let index = this.sections.indexOf(section);
+    if (index != -1) {
+      this.sections.splice(index, 1);
+    }
+  }
+
+  //Renaming and updating a section
+  UpdateSection(rename: Section, index: any) {
+    //throw new Error('Method not implemented.');
+    if(index!=-1){
+      this.sections[index]=rename;
+
+    }
   }
 
 }
