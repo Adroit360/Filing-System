fedb463786a52c7107817fcd83ec9473c94cf481
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2020-10-28 10:04:05 -0700

    commit

--- src/app/content/permissions/received/received.component.ts
+++ src/app/content/permissions/received/received.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./received.component.scss']
 })
 export class ReceivedComponent implements OnInit {
+ receivedPermissionDetails =[{email: 'nadum@adroit360.com', date: '18/09/2020'},
+  {email: 'nadum@adroit360.com', date: '18/09/2020'},
+  {email: 'nadum@adroit360.com', date: '18/09/2020'},
+  {email: 'nadum@adroit360.com', date: '18/09/2020'},
+  {email: 'nadum@adroit360.com', date: '18/09/2020'},
+  {email: 'nadum@adroit360.com', date: '18/09/2020'},
+  {email: 'nadum@adroit360.com', date: '18/09/2020'},
+  {email: 'nadum@adroit360.com', date: '18/09/2020'},
+  {email: 'nadum@adroit360.com', date: '18/09/2020'},
+  {email: 'nadum@adroit360.com', date: '18/09/2020'},
+  {email: 'nadum@adroit360.com', date: '18/09/2020'},
 
+]
   constructor() { }
 
   ngOnInit(): void {
