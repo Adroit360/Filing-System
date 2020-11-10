be802fa163d9c061e1c591acc9b04422fb149cea
emmaofori20 <emmanuelofori2638@gmail.com>
2020-11-10 15:54:53 +0000

    changes

---
+++ src/app/tilte-case.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TilteCasePipe } from './tilte-case.pipe';
+
+describe('TilteCasePipe', () => {
+  it('create an instance', () => {
+    const pipe = new TilteCasePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
